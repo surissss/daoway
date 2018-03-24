7981f85782b909c4b4e60463907f775a6f4422d1
surisushine <surisunshine1211@163.com>
2018-03-24 11:09:56 +0800

    daoway 第三天

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,36 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
+const database = require('../models/index.js')
 
+
+const title = '到位APP - 高品质上门服务直约平台, 家政服务, 上门按摩, 上门维修, 搬家, 安心到家!'
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  database.shop.find({},function (err,result) {
+    if (!err){
+      res.render('index',{title,result});
+    }
+  })
+
+});
+
+router.get('/service', function(req, res, next) {
+  database.service.find({},function (err,result) {
+    if (!err){
+      res.render('service',{title,result});
+    }
+  })
+
+});
+
+
+router.get('/seller', function(req, res, next) {
+  database.item.find({},function (err,result) {
+    if (!err){
+      res.render('seller',{title,result});
+    }
+  })
+
 });
 
 module.exports = router;
